088c1d118314657e1591db2c4a1bf5d300d2c718
Kingston Whyte <kingstonwhyte7@gmail.com>
2026-03-30 14:55:41 +0100

    wrote some boilerplate code

--- AGENT.md
+++ AGENT.md
@@ -2,4 +2,7 @@ Build a simple quiz app using fastapi,
 You should be able to select what topic you want to practice, receive a round of randomly ordered questions
 It should track your score for each question, total scores overall etc.
 
-Modern, lightweight, dark theme togglable interface. Keep the app simple for now
+Modern, lightweight, dark theme togglable interface. Keep the app simple for now.
+
+You should also base the other templates off of the layout.html, which should have a navbar and nice looking css.
+
--- app.py
+++ app.py
@@ -1,8 +1,33 @@
 from typing import Annotated
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-import uvicorn
+import uvicorn, yaml, json
 
-# this'll be fastapi
+with open("./config.yml") as f:
+    config = yaml.safe_load(f)
 
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+def load_question_data():
+    with open("./data/full_questions.json") as f:
+        return json.load(f)
+
+question_data = load_question_data()
+
+@app.get("/"):
+def _index(request: Request):
+    templates.TemplateResponse(
+        request,
+        "topic_selection.html"
+    )
+
+def main():
+    uvicorn.run(
+        app,
+        **config.get("uvicorn")
+    )
+
+if __name__ == "__main__":
+    main()
 
